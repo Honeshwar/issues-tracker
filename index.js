b780f28eae32b87f7dac8da19daf9a5ef177ad86
Honeshwar Thakur <st566654@gmail.com>
2023-06-27 18:16:18 +0530

    home page created UI

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+ 
 //plane nodejs server
 // const http = require('http');
 // const server = http.createServer();
@@ -15,11 +15,39 @@
 
 const express = require("express");
 const app = express();
+//MW to decode req
+app.use(express.urlencoded());
+
+//to setup ejs
+const path = require('path');
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,"views"));//path views folder give to template engine , so it can find .render(file)
+
+//MW for assets static file provide
+app.use(express.static(path.join(__dirname,"/assets")));
+
+app.get('/',(req,res)=>{
+    res.render('home',{
+        projects:[
+            {title:"movie cart",author:"Xenim"},
+            {title:"shop cart",author:"hon"},
+            {title:"lens cart",author:"yon"},
+            {title:"food cart",author:"ok"},
+            {title:"shoes cart",author:"zin"},
+            {title:"movie cart",author:"Xenim"},
+            {title:"shop cart",author:"hon"},
+            {title:"lens cart",author:"yon"},
+            {title:"food cart",author:"ok"},
+            {title:"shoes cart",author:"zin"}
+        ]
+    })
+})
+
 
 app.listen(8000,(e)=>{
     if(e){
         console.log("error",error);
         return;
     }
-    console.log("server running on port: 5000");
+    console.log("server running on port: 8000");
 });
